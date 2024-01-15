ee0cef2f54556733ee1561999b79fadf3103a2ea
Zach Watts <zach@zachwatts.online>
2024-01-15 11:06:14 -0500

    updates router config to use latest version of react-router-dom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 // Либо использовать @loadable/component, в рамках туториала - некритично
 import {lazy} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 const TasksListPage = lazy(() => import('./tasks-list'));
 const TaskDetailsPage = lazy(() => import('./task-details'));
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<TasksListPage />}>
+      <Route path="/:taskId" element={<TaskDetailsPage />} />
+    </Route>
+  )
+);
+
 export const Routing = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={TasksListPage} />
-      <Route exact path="/:taskId" component={TaskDetailsPage} />
-      <Redirect to="/" />
-    </Switch>
+    <RouterProvider router={router} />
+
+    // <Switch>
+    //   <Route exact path="/" component={TasksListPage} />
+    //   <Route exact path="/:taskId" component={TaskDetailsPage} />
+    //   <Redirect to="/" />
+    // </Switch>
   );
 };
