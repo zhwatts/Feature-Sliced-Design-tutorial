97c14f66f9683f15b0dfb4dc5ca3633c0da615dd
Zach Watts <zach@zachwatts.online>
2024-01-15 11:00:18 -0500

    init commit - creates vite project, and copies src folder from FSD sandbox

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import {Routing} from 'pages';
+// import {withProviders} from './providers';
+import './index.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Routing />
+    </div>
+  );
+};
+
+// export default withProviders(App);
+export default App;
