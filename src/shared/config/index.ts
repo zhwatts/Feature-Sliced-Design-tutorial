2858e2eeeceb8be333ae16ea84c4bd86901fcbf8
Zach Watts <zach@zachwatts.online>
2024-01-15 11:26:13 -0500

    updates environment variable references to support Vite

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,27 +1,29 @@
 /**
- * Модуль инициализации env-переменных
- * @remark Если не найдено значение хоть одной переменной,
- * Приложение сразу выбросит ошибку, при инициализации модуля
+ * Module for initializing env variables
+ * @remark If the value of any variable is not found,
+ * the application will immediately throw an error during module initialization
  * @module
  */
 
 /**
- * Получение env-переменной
+ * Get env variable
  * @throwable
  */
- const getEnvVar = (key: string) => {
-    if (process.env[key] === undefined) {
-        throw new Error(`Env variable ${key} is required`);
-    }
-    return process.env[key] || "";
+console.log('ENV VARS === ', import.meta.env);
+
+const getEnvVar = (key: string) => {
+  if (import.meta.env[key] === undefined) {
+    throw new Error(`Env variable ${key} is required`);
+  }
+  return import.meta.env[key] || '';
 };
 
 /** API entrypoint */
-export const API_URL = getEnvVar("REACT_APP_API_URL");
+export const API_URL = getEnvVar('VITE_API_URL');
 
-/** Режим запуска программы */
-export const NODE_ENV = getEnvVar("NODE_ENV");
-/** Режим разработки */
-export const isDevEnv = NODE_ENV === "development";
-/** Режим продакшена */
-export const isProdEnv = NODE_ENV === "production";
+/** Program execution mode */
+export const NODE_ENV = getEnvVar('MODE');
+/** Development mode */
+export const isDevEnv = NODE_ENV === 'development';
+/** Production mode */
+export const isProdEnv = NODE_ENV === 'production';
